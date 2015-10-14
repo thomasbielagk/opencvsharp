606d0c9203e35a098b9b100b3c857340bf95c130
shimat <schimatk@gmail.com>
2015-10-14 22:18:14 +0900

    fixed #135

--- src/OpenCvSharp.CPlusPlus/modules/core/PCA.cs
+++ src/OpenCvSharp.CPlusPlus/modules/core/PCA.cs
@@ -120,7 +120,7 @@ namespace OpenCvSharp.CPlusPlus
             {
                 if (disposed)
                     throw new ObjectDisposedException("PCA");
-                IntPtr ret = NativeMethods.core_PCA_eigenvalues(ptr);
+                IntPtr ret = NativeMethods.core_PCA_mean(ptr);
                 return new Mat(ret);
             }
         }
